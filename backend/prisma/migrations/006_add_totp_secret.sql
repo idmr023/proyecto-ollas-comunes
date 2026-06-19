4176de1f487a6d97d43da0ae897e986020a78be4
Ivan Manrique <jparatupcya@gmail.com>
2026-06-18 20:59:57 -0500

    Mejoras a nivel de UX

--- backend/prisma/migrations/006_add_totp_secret.sql
+++
@@ -1,4 +0,0 @@
--- Migration: 006_add_totp_secret.sql
--- Agrega el campo totp_secret a la tabla app_users para TOTP (autenticación en dos pasos sin email)
-
-ALTER TABLE public.app_users ADD COLUMN IF NOT EXISTS totp_secret varchar(255);
--- backend/src/modules/ollas-comunes/errors.ts
+++
@@ -1,9 +0,0 @@
-export class OllaServiceError extends Error {
-  statusCode: number
-
-  constructor(statusCode: number, message: string) {
-    super(message)
-    this.name = 'OllaServiceError'
-    this.statusCode = statusCode
-  }
-}
