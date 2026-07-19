a2152dba18c8c3ae08c8d6a975c6fe743b6e55b3
Francesco <148834578+Cheskito@users.noreply.github.com>
2026-07-18 21:48:17 -0500

    Hot fix playwright config

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://proyecto-ollas-comunes.vercel.app/',
+    baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
@@ -39,10 +39,7 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+
   ],
 
 })
